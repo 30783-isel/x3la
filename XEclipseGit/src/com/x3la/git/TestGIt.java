828239b697dbc610c9c48fda16bfc8c1477b2186
Paulo Estrela <janganga@gmail.com>
2020-04-01 18:57:31 +0100

    Primeiro commit

---
+++ XEclipseGit/src/com/x3la/git/TestGIt.java
@@ -0,0 +1,12 @@
+package com.x3la.git;
+
+public class TestGIt {
+
+	public static void main(String[] args) {
+		String[] names = {"Bilbo", "Frodo", "Aragorn", "Legolas", "Gandalf"};
+		
+		for(int i = 0; i < names.length; i++) {
+			System.out.format("Hello, %s \n", names[i]);
+		}
+	}
+}
