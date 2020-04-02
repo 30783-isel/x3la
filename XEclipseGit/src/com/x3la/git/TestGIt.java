39d1f67c7ad1e02646932c16a95d1903b0f79884
Paulo Janganga <pjanganga.edge@scml.pt>
2020-04-02 13:50:51 +0100

    terceiro commit - adicionado dois names e um print

--- XEclipseGit/src/com/x3la/git/TestGIt.java
+++ XEclipseGit/src/com/x3la/git/TestGIt.java
@@ -3,10 +3,11 @@ package com.x3la.git;
 public class TestGIt {
 
 	public static void main(String[] args) {
-		String[] names = {"Bilbo", "Frodo", "Aragorn", "Legolas", "Gandalf"};
+		String[] names = {"Bilbo", "Frodo", "Aragorn", "Legolas", "Gandalf", "Boromir", "Faramir"};
 		
 		for(int i = 0; i < names.length; i++) {
 			System.out.format("%s - Hello, %s \n", String.valueOf(i), names[i]);
+			System.out.println("What the hell you gonna do today.");
 		}
 	}
 }
