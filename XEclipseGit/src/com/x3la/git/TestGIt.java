c7869a491f94ff622920eeba0c65f36b93796147
Paulo Janganga <pjanganga.edge@scml.pt>
2020-04-06 19:25:49 +0100

    Change A

--- XEclipseGit/src/com/x3la/git/TestGIt.java
+++ XEclipseGit/src/com/x3la/git/TestGIt.java
@@ -2,6 +2,8 @@ package com.x3la.git;
 
 public class TestGIt {
 
+	//Change A
+	
 	public static void main(String[] args) {
 		String[] names = {"Bilbo", "Frodo", "Aragorn", "Legolas", "Gandalf", "Boromir", "Faramir"};
 		
